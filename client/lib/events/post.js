e8c89051842b21d86118665c0031c3f58ec3a348
Everton Mendonça <evertonmj@gmail.com>
2016-02-13 15:20:49 -0300

    Adição de login

---
+++ client/lib/events/post.js
@@ -0,0 +1,9 @@
+Template.post.events({
+	"submit form": function(e, template) {
+		e.preventDefault();
+		var textarea = template.find("textarea");
+		Post.insert({message: textarea.value});
+		
+		textarea.value = "";
+	}
+});
